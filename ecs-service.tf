3784daef60932107e2dc3ccffaa247b625eec956
Kingsley <kingsleychinono@gmail.com>
2025-09-25 22:12:56 +0100

    added ecs-service.tf

---
+++ ecs-service.tf
@@ -0,0 +1,14 @@
+# ECS Service
+resource "aws_ecs_service" "java-app" {
+  name            = "java-app-container"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.nginx.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+    assign_public_ip = true
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+}
